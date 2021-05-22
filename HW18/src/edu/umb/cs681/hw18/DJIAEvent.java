43d5048532044a40be9a51a4d7b78738a43bb52c
Smit Patel <smit.patel001@umb.edu>
2021-05-21 22:41:25 -0400

    Delete DJIAEvent.java

--- HW18/src/edu/umb/cs681/hw18/DJIAEvent.java
+++
@@ -1,8 +0,0 @@
-package edu.umb.cs681.hw18;
-
-public class DJIAEvent {
-    private float djia;
-	public float getDjia() {	return djia;	}
-	public DJIAEvent(float djia){	this.djia = djia;	}
-	public void setDjia(float djia) {	this.djia = djia;	}
-}
